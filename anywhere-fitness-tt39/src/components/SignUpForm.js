0356a63dd3425c3f3a7cd740546b209ad02542ab
CarlosSantiago Nevett <csnevett@gmail.com>
2021-02-01 18:13:10 -0500

    bare commit

--- anywhere-fitness-tt39/src/components/SignUpForm.js
+++ anywhere-fitness-tt39/src/components/SignUpForm.js
@@ -1,1 +1,9 @@
 import React from 'react'
+
+export default function SignUpForm(props) {
+    const {formValues, change, submit, buttonDisabled, errors} = props;
+
+    const onChange = (evt) => {
+        const {name, type, checked, value} = evt.target;
+     }
+}
