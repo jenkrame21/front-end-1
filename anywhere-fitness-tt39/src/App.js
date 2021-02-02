3db84b1a74b1e50ff29fe1e78b4deac1d03ad408
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2021-02-02 07:15:14 -0700

    Added axiosWithAuth, added Private Route

--- anywhere-fitness-tt39/src/App.js
+++ anywhere-fitness-tt39/src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 
+
 function App() {
+  const [loggedIn, setLoggedIn] = useState(window.localStorage.getItem('token'));
+
   return (
     <div className="App">
       <h1>Anywhere Fitness</h1>
+
+
     </div>
   );
 }
