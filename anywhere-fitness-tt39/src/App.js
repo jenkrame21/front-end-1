720ebf1bfd931a48ff4f01365de6e83bd740b26c
CarlosSantiago Nevett <csnevett@gmail.com>
2021-02-02 10:54:56 -0500

    LoginForm complete with fake api

--- anywhere-fitness-tt39/src/App.js
+++ anywhere-fitness-tt39/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-// import SignUpForm from './components/SignUpForm'
+import SignUpForm from './components/SignUpForm'
+import LoginForm from './components/LoginForm'
 
 function App() {
   
   return (
     <div className="App">
       <h1>Anywhere Fitness</h1>
+      <SignUpForm/>
+      <br/>
+      <LoginForm/>
     </div>
   );
 }
---
+++ anywhere-fitness-tt39/src/validation/loginFormSchema.js
@@ -0,0 +1,13 @@
+import * as yup from 'yup'
+
+export default yup.object().shape({
+  username: yup.string()
+    .required('Must include username')
+    .min(4, 'Username must be at least four characters long.'),
+  password: yup.string()
+    .required('Password is required.')
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/g,
+      'Password must contain capital, lowercase, number & have a min. length of 8'
+    )
+})
